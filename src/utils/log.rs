43cb2c38ff84ef56756196c0ef422a7a80f36887
17ms <79069176+17ms@users.noreply.github.com>
2022-11-23 13:36:39 +0200

    renamed logs output folder & cleaned .gitignore

--- src/utils/log.rs
+++ src/utils/log.rs
@@ -9,7 +9,7 @@ use log4rs::{
 pub fn init_logger() -> Result<(), Box<dyn std::error::Error>> {
     let logfile = FileAppender::builder()
         .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
-        .build("log/output.log")?;
+        .build("logs/output.log")?;
     let config = Config::builder()
         .appender(Appender::builder().build("logfile", Box::new(logfile)))
         .build(Root::builder().appender("logfile").build(LevelFilter::Info))?;
