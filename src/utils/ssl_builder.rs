26134e5b74a1125d806838a343bc62e51c214bcc
17ms <79069176+17ms@users.noreply.github.com>
2022-11-23 17:10:30 +0200

    include altname to SSL cert for compatibility

--- src/utils/ssl_builder.rs
+++ src/utils/ssl_builder.rs
@@ -1,7 +1,7 @@
 use openssl::ssl::{SslAcceptor, SslAcceptorBuilder, SslFiletype, SslMethod};
 
 pub fn create_builder() -> Result<SslAcceptorBuilder, Box<dyn std::error::Error>> {
-    // dev: openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out cert.pem
+    // dev: openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out cert.pem -addext "subjectAltName = DNS:localhost"
     // dev: manually configure browser to trust the created cert
     let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls())?;
     builder.set_private_key_file("key.pem", SslFiletype::PEM)?;
